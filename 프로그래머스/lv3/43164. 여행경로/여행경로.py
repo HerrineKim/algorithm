33537e14bf345a679df81539f264196f6b541ae1
hyerin <herrine.kim@gmail.com>
2023-02-20 13:59:18 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -4,22 +4,40 @@ def dfs(tickets, start, visited, path):
 
     for i, ticket in enumerate(tickets):
         if ticket[0] == start and not visited[i]:
-						# 가본 적 없는 곳이라면 visit 처리하고 
             visited[i] = True
             path.append(ticket[1])
             dfs(tickets, ticket[1], visited, path)
-						# 끝까지 가는데 성공했으면 여태까지 쌓인 path를 return
             if len(path) == len(tickets) + 1:
                 return path
-						# 쭉 따라 갔는데 잘못 갔다면 pop, unvisit 처리 해주고 다른 path 탐색
             path.pop()
             visited[i] = False
 
 def solution(tickets):
     answer = []
     visited = [False] * len(tickets)
-		# ICN 다음으로 올 것으로 알파벳 빠른 것을 놓기 위해 lambda식으로 정렬
-    tickets.sort(key=lambda x: x[1])
+    tickets.sort(key=lambda x: x[0])
     answer = dfs(tickets, "ICN", visited, ["ICN"])
 
     return answer
+
+# def bfs(tickets, start, visited, path):
+#     if len(path) == len(tickets) + 1:
+#         return path
+#     for i in range(len(tickets)):
+#         if tickets[i][0] == start and not visited[i]:
+#             visited[i] = True
+#             path.append(tickets[i][1])
+#             path = bfs(tickets, tickets[i][1], visited, path)
+#             if len(path) == len(tickets) + 1:
+#                 return path
+#             visited[i] = False
+#             path.pop()
+#     return path
+
+# def solution(tickets):
+#     answer = []
+#     visited = [False] * len(tickets)
+#     tickets.sort()
+#     answer = bfs(tickets, "ICN", visited, ["ICN"])
+
+#     return answer
