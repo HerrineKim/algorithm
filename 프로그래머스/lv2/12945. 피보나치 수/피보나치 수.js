fe802df521bbbd9185b79ed861f9ca1ab807ca82
hyerin <herrine.kim@gmail.com>
2023-03-19 20:48:11 +0900

    [level 2] Title: 피보나치 수, Time: 4.99 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,7 @@
 function solution(n) {
     var answer = 0;
-    // 시간 단축
     if (n === 1) return 1;
     if (n === 2) return 1;
-    // 피보나치 수열
     let arr = [1, 1];
     for (let i = 2; i < n; i++) {
         arr.push((arr[i - 1] + arr[i - 2]) % 1234567);
