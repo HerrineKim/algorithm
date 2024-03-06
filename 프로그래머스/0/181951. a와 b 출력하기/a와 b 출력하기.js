debc88162848ef821439f50a3392e24d3f39aeae
River <herrine.kim@gmail.com>
2024-03-07 01:54:32 +0900

    [level 0] Title: a와 b 출력하기, Time: 45.08 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    let a = 'a = '
+    let b = 'b = '
+    a += input[0]
+    b += input[1]
+    console.log(a)
+    console.log(b)
+});
